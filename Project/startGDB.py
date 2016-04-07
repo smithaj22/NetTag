a3f17e6ecf05cdd12e2c633436a3c5aeda67e92b
Austin Smith <pistonsfansince2000@wireless149-235.wireless.wmich.edu>
2016-04-07 19:29:01 -0400

    moved start GDB so it is in with the latest code

--- Project/startGDB.py
+++ Project/startGDB.py
@@ -1,9 +1,10 @@
 #program that starts a gdb session and loads a binary to the target.Target is left in halted state.
 import subprocess
 
-def startGDB(binPath):
+def startGDB(binPath,cfgPath):
+    subprocess sp=popen(['openocd','-f',cfgPath]);
     string="load "+binPath;
     subprocess.call("arm-none-eabi-gdb -ex 'target remote localhost:3333' -ex 'monitor reset halt' -ex "+string,shell=True);
     return;
-#test code 
+#test code
 #startGDB("/home/greg/CS3950/HelloArm.elf");
