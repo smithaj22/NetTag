83121a038a111ffc2c0cf3da5366707e72116a7e
Austin Smith <ajsmith@Austins-iMac.local>
2016-04-27 20:10:39 -0400

    moved Jimenas latest programs to the Project Folder

--- Project/New_file_list.py
+++ Project/New_file_list.py
@@ -5,82 +5,91 @@ in a combo box the available binary files that can be flashed to this board
 using binaries_available.py.
 It also dislays an option to run or debug the program and then a button to
 start the process to flash the file to run or debug it.'''
-#new comment
+
 # example radiobuttons.py
 import pygtk
 pygtk.require('2.0')
 import gtk
 import binaries_available
+import os
 #import startGDB
 
 binary_selected = "0"
+board_selected = "board1"
 
 class OptionWindow:
 
-    def close_application(self, widget, event, data=None):
+    def close_application(self, widget):
         #Change this to maybe turn off NetTag
-        gtk.main_quit()
+        os.system("sudo shutdown -h now")
+	#gtk.main_quit()
         return False
 
     def change_bin_file(self, widget):
-        binary_selected = widget.get_active_text()
-        message = str(binary_selected) +" file selected\nNow select Flash or Debug"
-        self.upperLabel.set_text(message)
+        binary_selected = "./" + str(widget.get_active_text())
+        message = "File selected:  " + str(widget.get_active_text())
+        self.fileLabel.set_text(message)
+	self.extraLabel.set_text("Now select Flash or Debug")
         print str(binary_selected) + " selected"
-
+	
     def text_change(self, widget):
         # bin_ava_binary_to_flash = widget.get_active_text()
         print "text changed"
 
     def flash_to_board(self, widget):
         print "file will now flash"
-        ####Path hardcoded for now
-        startGDB.startGDB(binaries_available.binary_exe[0])
+	#Add call to FLASH CODE
+       	startGDB.startGDB(binary_selected)
 
     def debug(self, widget, data=None):
+	#Add call to DEBUG CODE
         print "debug function will be called"
 
-
     def __init__(self):
-        ####window settings###
+	message = " " 
+	binaries_available.binAvailable(board_selected)
+       ####window settings###
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        gtk.Window.fullscreen(self.window)
+	gtk.Window.fullscreen(self.window)
         # self.window.set_position(gtk.WIN_POS_CENTER)
         self.window.connect("delete_event", self.close_application)
         self.window.set_size_request(240,150)
         self.window.set_title("NetTag")
         self.box = gtk.VBox()
-        self.window.maximize()
+#        self.window.maximize()
 
         ####Label###
-        self.upperLabel = gtk.Label("\tSelect a file\nThen Flash or Debug")
-        self.box.pack_start(self.upperLabel)
+        self.upperLabel = gtk.Label("\tSelect a file")
+        self.upperLabel.set_text("Board found:  " + str(board_selected))
+		
+	#Exit button
+	self.upperBox = gtk.HBox()
+	self.exitButton = gtk.Button("X")
+	self.exitButton.connect("clicked" , self.close_application)
+	self.upperBox.pack_start(self.exitButton)
 
-        ##List for comboBox -  http://stackoverflow.com/questions/23757738/python-gtk-how-to-insert-items-from-list-to-combobox
-        
-        self.fileList = gtk.ListStore(str)
-        files = binaries_available.binary_names
+	self.upperBox.pack_start(self.upperLabel)
 
-        for row in files:
-            self.fileList.append([row[0]])
-        cell = gtk.CellRendererText()
-        comboB = gtk.ComboBox(model=self.fileList)
-        self.box.pack_start(cell)
-        comboB.set_attributes(cell, text = 0)
+	self.box.pack_start(self.upperBox)
+	#file label button
+	self.fileLabel = gtk.Label("Select file")
+	self.fileLabel.set_text("Select File")	
+	self.box.pack_start(self.fileLabel)
 
-
+	#Run or debug button
+	self.extraLabel = gtk.Label(" ");
 
 
         ####Combo box###
-        # '''having trouble clicking on the combo box when it shows on GUI'''
-        # self.comboList = gtk.combo_box_new_text()
-        # self.comboList.connect("changed", self.change_bin_file)
-        # self.comboList.set_active(0)
-        # self.comboList.append_text("Text 0")
-        # self.comboList.append_text("Text 1")
-        # self.comboList.append_text("Text 2")
+        self.comboList = gtk.combo_box_new_text()
+        self.comboList.connect("changed", self.change_bin_file)
+#        self.comboList.set_active(0)
 
-        # self.box.pack_start(self.comboList, False, False)
+        for indexFile in range(len(binaries_available.binary_names)):
+            self.comboList.append_text(binaries_available.binary_names[indexFile])
+            #print 'File: ', binaries_available.binary_names[indexFile]
+	    
+        self.box.pack_start(self.comboList, False, False)
 
         ###Two buttons (to run or debug)###
         self.twoButtonBox = gtk.HBox()
@@ -93,6 +102,7 @@ class OptionWindow:
         self.DebugButton.connect("clicked", self.debug)
         self.twoButtonBox.pack_start(self.DebugButton)
 
+	self.box.pack_start(self.extraLabel)
         self.box.pack_start(self.twoButtonBox)
 
         self.window.add(self.box)
--- Project/main_window.py
+++ Project/main_window.py
@@ -9,55 +9,108 @@ pygtk.require('2.0')
 import gtk
 import get_id
 import New_file_list
+import os
 
 board = "will get Id from get_id"
+target_selected = "file manually selected"
 binary_file = "bin file to flasH"
 runOrDebug = 0 #0 for run 1 for debug
 
 class StartWin:
     def callback_getId(self, widget):
-        print "Start was selected.."
+	New_file_list.board_selected = "board0" 
+	print New_file_list.board_selected
+       	print "Start was selected.."
         board = get_id.findBoard()
         print board
+#	New_file_list.board_selected = board
+        New_file_list.OptionWindow()
+
+    def callback_file_window(self, widget):
+        New_file_list.board_selected = target_selected
+        print New_file_list.board_selected
+        print "Board was selected.."
+	self.manualStartButton.set_text(str(target_selected))
         New_file_list.OptionWindow()
 
 
-    # def shut_down_pi(self, widget):
-    #     print "NetTag will now shut down.."
-
-    def close_application(self, widget, event, data=None):
+    def change_target(self, widget):
+	target_selected = str(widget.get_active_text())
+    
+    def close_application(self, widget):
         #Change this to maybe turn off NetTag
-        gtk.main_quit()
+        os.system("sudo shutdown -h now")
+	gtk.main_quit()
         return False
 
     def __init__(self):
         #window settings
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	gtk.Window.fullscreen(self.window)
         # self.window.set_position(gtk.WIN_POS_CENTER)
         self.window.connect("delete_event", self.close_application)
         self.window.set_size_request(480,250)
         self.window.set_title("NetTag")
+	
 
         #Logo - inserted as image
         self.logo = gtk.Image()
         self.logo.set_from_file("Pics/miniLogo.png")
         # self.logo.queue_resize
+	
+	####Combo box###
+        self.comboList = gtk.combo_box_new_text()
+        self.comboList.connect("changed", self.change_target)
+#        self.comboList.set_active(0)
 
-        #Button
-        self.startButton = gtk.Button("Start")
+#        for indexFile in range(len(binaries_available.binary_names)):
+#            self.comboList.append_text(binaries_available.binary_names[indexFile])
+#            #print 'File: ', binaries_available.binary_names$
+		
+        self.comboList.append_text("STM ")
+        self.comboList.append_text("TI ")
+
+
+
+	
+        #Button- auto start
+        self.startButton = gtk.Button("Search for board")
         self.startButton.connect("clicked", self.callback_getId)
         self.startButton.set_tooltip_text("Click to find board")
 
-        # self.shutDown = gtk.Button("ShutDown")
-        # self.shutDown.connect("clicked", self.shut_down_pi)
+	#Button- manual start
+        self.manualStartButton = gtk.Button("Select board")
+        self.manualStartButton.connect("clicked", self.callback_file_window)
+        
+        
+
+	#exit button and logo
+	self.exitBox = gtk.HBox()
+
+	self.exitButton = gtk.Button(" X ")
+	self.exitButton.connect("clicked", self.close_application)
+	self.exitBox.pack_start(self.exitButton)
+
+	self.welcomeLabel = gtk.Label("Welcome! Connect board then select 'Start'")	
+	self.exitBox.pack_start(self.welcomeLabel)
+
 
         self.box = gtk.VBox()
+	self.box.pack_start(self.exitBox)
         self.box.pack_start(self.logo)
-        self.box.pack_start(self.startButton)
+        self.box.pack_start(self.comboList, False, False)
+
+	self.startBox = gtk.HBox()
+	
+        self.startBox.pack_start(self.startButton)
+	self.startBox.pack_start(self.manualStartButton)
+	self.box.pack_start(self.startBox)
         # self.box.pack_start(self.shut_down_pi)
 
         #add and display on window
         self.window.add(self.box)
+	#Added here
+#	self.window.add(self.exitButton)
         self.window.set_border_width(0)
         self.window.show_all()
 
